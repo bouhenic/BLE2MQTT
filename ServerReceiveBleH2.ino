3473d435ef1488b769aadb2e996fbc003b7a85fc
bouhenic <39024540+bouhenic@users.noreply.github.com>
2024-02-20 20:42:51 +0100

    Update ServerReceiveBleH2.ino
    Gestion des déconnexions.

--- ServerReceiveBleH2.ino
+++ ServerReceiveBleH2.ino
@@ -1,40 +1,63 @@
 #include <BLEDevice.h>
-#include <BLEServer.h>
 #include <BLEUtils.h>
+#include <BLEServer.h>
 #include <BLE2902.h>
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
+bool shouldAdvertise = true; // Indicateur pour contrôler la publicité
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) override {
+        Serial.println("Client Connected");
+        shouldAdvertise = false; // Arrêter la publicité lorsqu'un client est connecté
+    }
+
+    void onDisconnect(BLEServer* pServer) override {
+        Serial.println("Client Disconnected");
+        shouldAdvertise = true; // Indiquer que la publicité doit être redémarrée
+    }
+};
+
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) override {
-        // Utiliser directement la valeur retournée par getValue()
         auto value = pCharacteristic->getValue();
 
         Serial.print("Received Value: ");
-        // Affichage des données reçues
         for (size_t i = 0; i < value.length(); i++) {
             Serial.print((char)value[i]);
         }
         Serial.println();
+
+        if (value == "arrosage") {
+            Serial.println("Activating pin 2 for irrigation!");
+            digitalWrite(2, HIGH);
+        } else if (value == "stop") {
+            Serial.println("Deactivating pin 2.");
+            digitalWrite(2, LOW);
+        } else {
+            Serial.println("Bad order");
+        }
     }
 };
-
-
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
 
+  pinMode(2, OUTPUT); // Configure le pin 2 comme sortie
+  digitalWrite(2, LOW); // Initialise le pin 2 à l'état bas
+
   BLEDevice::init("MyBleDevice");
   BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks()); // Enregistrer les callbacks de serveur
 
   BLEService *pService = pServer->createService(SERVICE_UUID);
   BLECharacteristic *pCharacteristic = pService->createCharacteristic(
                                           CHARACTERISTIC_UUID,
                                           BLECharacteristic::PROPERTY_READ |
-                                          BLECharacteristic::PROPERTY_WRITE
-                                      );
+                                          BLECharacteristic::PROPERTY_WRITE);
 
   pCharacteristic->setCallbacks(new MyCallbacks());
   pCharacteristic->setValue("Hello World says Neil");
@@ -45,15 +68,18 @@ void setup() {
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);  // Functions that help with iPhone connections issue
-  pAdvertising->setMaxPreferred(0x12);
   BLEDevice::startAdvertising();
   Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
 void loop() {
-  // Here, we simply wait and let the BLE callbacks handle the data.
-  delay(1000);
+  // Vérifie si la publicité doit être redémarrée
+  if (shouldAdvertise) {
+      BLEDevice::startAdvertising(); // Redémarre directement la publicité
+      Serial.println("Restarting advertising...");
+  }
+  delay(1000); // Attente passive et laisse les callbacks BLE gérer les données.
 }
+
 
 
