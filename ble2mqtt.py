2f9a0a7ce19216596e8d327d9325fce1ece58d8b
bouhenic <39024540+bouhenic@users.noreply.github.com>
2024-02-21 10:59:16 +0100

    Update ble2mqtt.py
    Paramètres de fonctionnement configurées à l'aide d'un fichier yaml.

--- ble2mqtt.py
+++ ble2mqtt.py
@@ -1,14 +1,30 @@
 import time
 from bluepy import btle
 from paho.mqtt import client as mqtt_client
+import yaml
 
-broker_address = "localhost"
-port = 1883
-topic = "your/topic"
-client_id = "your_client_id"
-# Si votre broker MQTT requiert une authentification
-mqtt_username = "your_username"
-mqtt_password = "your_password"
+def load_config(file_path):
+    with open(file_path, 'r') as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            return None
+
+# Charger la configuration
+config = load_config('config.yaml')
+
+# Utiliser les valeurs du fichier YAML
+broker_address = config['mqtt']['broker_address']
+port = config['mqtt']['port']
+topic = config['mqtt']['topic']
+client_id = config['mqtt']['client_id']
+mqtt_username = config['mqtt']['username']
+mqtt_password = config['mqtt']['password']
+device_address = config['ble']['device_address']
+service_uuid = btle.UUID(config['ble']['service_uuid'])
+characteristic_uuid = btle.UUID(config['ble']['characteristic_uuid'])
+
 
 class MyDelegate(btle.DefaultDelegate):
     def __init__(self):
@@ -16,7 +32,7 @@ class MyDelegate(btle.DefaultDelegate):
 
     def handleNotification(self, cHandle, data):
         print("Data received:", data)
-        # Ici, les données reçues de BLE sont simplement imprimées et non publiées sur MQTT
+        # Les données reçues de BLE sont simplement imprimées et non publiées sur MQTT
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -24,6 +40,11 @@ def on_connect(client, userdata, flags, rc):
         client.subscribe(topic)  # S'abonner au topic pour recevoir les messages
     else:
         print("Failed to connect, return code %d\n", rc)
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print("Unexpected MQTT disconnection. Attempting to reconnect.")
+        client.reconnect()
 
 def on_message(client, userdata, msg):
     print(f"MQTT Message received: {msg.payload}")
@@ -38,6 +59,7 @@ def connect_to_mqtt():
     client = mqtt_client.Client(client_id)
     client.username_pw_set(mqtt_username, mqtt_password)
     client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
     client.on_message = on_message
     client.connect(broker_address, port)
     client.loop_start()
@@ -47,27 +69,23 @@ mqtt_client = connect_to_mqtt()
 
 while True:
     try:
-        device_address = "48:31:B7:C0:35:0B"
         print("Trying to connect to BLE device...")
         dev = btle.Peripheral(device_address)
         dev.setDelegate(MyDelegate())
         print("Connected to BLE device")
         
-        service_uuid = btle.UUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b")
-        characteristic_uuid = btle.UUID("beb5483e-36e1-4688-b7f5-ea07361b26a8")
         svc = dev.getServiceByUUID(service_uuid)
         ch = svc.getCharacteristics(characteristic_uuid)[0]
         
         while True:
             if dev.waitForNotifications(1.0):
-                # handleNotification() a été appelé
-                continue
+                continue  # handleNotification() a été appelé
             print("Waiting for notifications...")
     except btle.BTLEException as e:
         print(f"BLE connection error: {e}, trying to reconnect...")
         time.sleep(5)  # Attendre avant de retenter une connexion
     except KeyboardInterrupt:
         print("Disconnecting...")
-        dev.disconnect()
+        if dev:
+            dev.disconnect()
         break
-
