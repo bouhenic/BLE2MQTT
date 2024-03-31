e86dde0e9eed64a9ed093f3e1d87b8545c6165aa
bouhenic <39024540+bouhenic@users.noreply.github.com>
2024-03-31 14:54:48 +0200

    Update ServerReceiveBleH2.ino

--- ServerReceiveBleH2.ino
+++ ServerReceiveBleH2.ino
@@ -11,15 +11,13 @@ bool deviceConnected = false;
 bool oldDeviceConnected = false;
 uint32_t value = 0;
 
-const int relayPin = 2; // Use the appropriate GPIO pin for your setup
-const int soilPin = 1;
+const int relayPin = 2; // Utilisez le GPIO approprié pour votre configuration
+const int soilPin = 1;  // Capteur d'humidité du sol
 
-// Valeur du potentiomètre
+// Valeur du capteur d'humidité du sol
 int soilValue = 0;
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
-
+// UUIDs pour les services et les caractéristiques
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define SENSOR_CHARACTERISTIC_UUID "beb5483d-36e1-4688-b7f5-ea07361b26a8"
 #define RELAY_CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
@@ -39,7 +37,7 @@ class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
         auto value = pRelayCharacteristic->getValue();
         if (value.length() > 0) {
             Serial.print("Characteristic event, written: ");
-            Serial.println(static_cast<int>(value[0])); // Print the integer value
+            Serial.println(static_cast<int>(value[0])); // Affiche la valeur entière
 
             int receivedValue = static_cast<int>(value[0]);
             if (receivedValue == 1) {
@@ -56,19 +54,19 @@ class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
 void setup() {
   Serial.begin(115200);
   pinMode(relayPin, OUTPUT);
-  pinMode(soilPin,INPUT_PULLUP);
+  pinMode(soilPin, INPUT_PULLUP);
 
-  // Create the BLE Device
+  // Création du dispositif BLE
   BLEDevice::init("MyBleDevice");
 
-  // Create the BLE Server
+  // Création du serveur BLE
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
-  // Create the BLE Service
+  // Création du service BLE
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
-  // Create a BLE Characteristic
+  // Création de la caractéristique du capteur de sol
   pSensorCharacteristic = pService->createCharacteristic(
                       SENSOR_CHARACTERISTIC_UUID,
                       BLECharacteristic::PROPERTY_READ   |
@@ -77,30 +75,39 @@ void setup() {
                       BLECharacteristic::PROPERTY_INDICATE
                     );
 
-  // Create the ON button Characteristic
+  // Ajout d'un descripteur de nom pour la caractéristique du capteur de sol
+  BLEDescriptor pSensorCharacteristicNameDescriptor((uint16_t)0x2901);
+  pSensorCharacteristicNameDescriptor.setValue("Capteur de Sol");
+  pSensorCharacteristic->addDescriptor(&pSensorCharacteristicNameDescriptor);
+
+  // Création de la caractéristique pour le relais
   pRelayCharacteristic = pService->createCharacteristic(
                       RELAY_CHARACTERISTIC_UUID,
                       BLECharacteristic::PROPERTY_WRITE
                     );
 
-  // Register the callback for the ON button characteristic
+  // Ajout d'un descripteur de nom pour la caractéristique du relais
+  BLEDescriptor pRelayCharacteristicNameDescriptor((uint16_t)0x2901);
+  pRelayCharacteristicNameDescriptor.setValue("Relais");
+  pRelayCharacteristic->addDescriptor(&pRelayCharacteristicNameDescriptor);
+
+  // Enregistrement du rappel pour la caractéristique du relais
   pRelayCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
 
-  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
-  // Create a BLE Descriptor
+  // Ajout de descripteurs BLE 2902 pour activer les notifications
   pSensorCharacteristic->addDescriptor(new BLE2902());
   pRelayCharacteristic->addDescriptor(new BLE2902());
 
-  // Start the service
+  // Démarrage du service
   pService->start();
 
-  // Start advertising
+  // Démarrage de la publicité
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(false);
-  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  pAdvertising->setMinPreferred(0x0);  
   BLEDevice::startAdvertising();
-  Serial.println("Waiting a client connection to notify...");
+  Serial.println("En attente de connexion d'un client pour notifier...");
 }
 
 void loop() {
@@ -108,34 +115,19 @@ void loop() {
         pSensorCharacteristic->setValue(String(soilValue).c_str());
         pSensorCharacteristic->notify();
         soilValue = analogRead(soilPin);
-        Serial.print("New value notified: ");
+        Serial.print("Nouvelle valeur notifiée: ");
         Serial.println(soilValue);
         delay(6000); 
     }
 
     if (!deviceConnected && oldDeviceConnected) {
-    Serial.println("Device disconnected. Preparing for restart.");
-    delay(1000); // Délai pour permettre la fin de toutes les opérations en cours
-    esp_restart(); // Redémarre l'ESP32
-}
+    Serial.println("Appareil déconnecté. Préparation au redémarrage.");
+    delay(1000);
+    esp_restart(); // Redémarrage de l'ESP32
+    }
     if (deviceConnected && !oldDeviceConnected) {
-        Serial.println("Device Connected");
+        Serial.println("Appareil connecté.");
         oldDeviceConnected = deviceConnected;
     }
 }
-
-// Déplacez la configuration initiale du BLE dans une fonction appelée `setupBLE()`
-void setupBLE() {
-    BLEDevice::init("MyBleDevice");
-    pServer = BLEDevice::createServer();
-    pServer->setCallbacks(new MyServerCallbacks());
-
-    BLEService *pService = pServer->createService(SERVICE_UUID);
-    // Ajoutez les caractéristiques et les descripteurs comme dans `setup()`
-    // ...
-    pService->start();
-    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-    pAdvertising->start();
-}
-
 
